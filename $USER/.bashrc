5ffc18df99f4a5936e3664e3ebd8a5fb60b62f92
Laoujin <woutervs@hotmail.com>
2014-05-17 22:42:02 +0200

    initial commit

---
+++ $USER/.bashrc
@@ -0,0 +1,9 @@
+clear
+
+SHELLCONFIG_PATH="/C/unix/code/bash/shellconfig"
+
+source $SHELLCONFIG_PATH/bash_prompt.sh
+source $SHELLCONFIG_PATH/quick_links.sh
+
+sangu
+echo commands: sangu, autokey, unixcode, www
---
+++ quick_links.sh
@@ -0,0 +1,15 @@
+function sangu() {
+	cd /c/unix/code/TribalWars/sangupackage-source/
+}
+
+function autokey(){
+  cd /c/unix/code/Autohotkey/Projects/WinDevHelper/
+}
+
+function unixcode(){
+  cd /c/unix/code/
+}
+
+function www(){
+  cd /c/unix/wamp/www/
+}
