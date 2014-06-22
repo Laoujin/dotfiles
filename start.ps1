a6a78d77df452a607dbaec4db8b29fd7aa6922c2
Laoujin <woutervs@hotmail.com>
2014-06-22 12:06:58 +0200

    auto symlink for User\.gitconfig and User\.bashrc files

---
+++ start.ps1
@@ -0,0 +1,19 @@
+﻿cls
+
+################
+"## GIT BASH ##"
+################
+function createUserHomeLink($file)
+{
+	$destination = "$HOME\$file"
+	if (-not (Test-Path ($destination)))
+	{
+		$params = "mklink $destination $PSScriptRoot\Users\$file"
+		cmd /c $params
+	}
+}
+
+createUserHomeLink(".gitconfig")
+createUserHomeLink(".bashrc")
+
+
