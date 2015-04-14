bf07579321438366b4563266840861763a027c67
Laoujin <woutervs@hotmail.com>
2015-04-15 00:52:06 +0200

    linked apache httpd.conf

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -10,7 +10,7 @@ Write-Host "Install all software with Boxstarter"
 Write-Host "Url: https://gist.github.com/Laoujin/12f5d2f76d51ee6c0a49"
 Write-Host
 
-#& "$PSScriptRoot\links.ps1"
+& "$PSScriptRoot\links.ps1"
 
 #& "$PSScriptRoot\PowerShell\modules.ps1"
 
