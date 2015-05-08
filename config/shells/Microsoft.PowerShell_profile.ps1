6f9988b5c1bfc1aca3fb2c214b08905c83c1fb9b
Laoujin <woutervs@hotmail.com>
2015-05-08 23:49:02 +0200

    new goodies for powershell

--- config/shells/Microsoft.PowerShell_profile.ps1
+++ config/shells/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,42 @@
 
+Set-Alias vgrt vagrant
+
+# Easier Navigation: .., ..., ...., ....., and ~
+${function:~} = { Set-Location ~ }
+# PoSh won't allow ${function:..} because of an invalid path error, so...
+${function:Set-ParentLocation} = { Set-Location .. }; Set-Alias ".." Set-ParentLocation
+${function:...} = { Set-Location ..\.. }
+${function:....} = { Set-Location ..\..\.. }
+${function:.....} = { Set-Location ..\..\..\.. }
+${function:......} = { Set-Location ..\..\..\..\.. }
+
+function CreateAndSet-Directory([String] $path) { New-Item $path -ItemType Directory -ErrorAction SilentlyContinue; Set-Location $path}
+
+function Convert-ToDiskSize {
+	param ( $bytes, $precision='0' )
+	foreach ($size in ("B","K","M","G","T")) {
+		if (($bytes -lt 1000) -or ($size -eq "T")){
+				$bytes = ($bytes).tostring("F0" + "$precision")
+				return "${bytes}${size}"
+		}
+		else { $bytes /= 1KB }
+	}
+}
+
+# Determine size of a file or total size of a directory
+function Get-DiskUsage([string] $path=(Get-Location).Path) {
+	Convert-ToDiskSize `
+		( `
+			Get-ChildItem $path -recurse -ErrorAction SilentlyContinue `
+			| Measure-Object -property length -sum -ErrorAction SilentlyContinue
+		).Sum `
+	1
+}
+
+Set-Alias mkd CreateAndSet-Directory
+Set-Alias fs Get-DiskUsage
+
+
 Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
 if (Test-Path ".\Microsoft.PowerShell_aliases.ps1") {
