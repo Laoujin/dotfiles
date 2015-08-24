cf2e5cfa6360e4de5cb2d82b45dda92c3757282b
Laoujin <woutervs@hotmail.com>
2015-08-24 23:27:15 +0200

    Path: markdownpad2

--- config/shells/Microsoft.PowerShell_profile.ps1
+++ config/shells/Microsoft.PowerShell_profile.ps1
@@ -38,6 +38,7 @@ if (Test-Path ".\Microsoft.PowerShell_aliases.ps1") {
 }
 
 Append-EnvPath "C:\Program Files\Sublime Text 3"
+Append-EnvPath "C:\Program Files (x86)\MarkdownPad 2"
 
 Set-Alias vgrt vagrant
 Set-Alias mkd CreateAndSet-Directory
