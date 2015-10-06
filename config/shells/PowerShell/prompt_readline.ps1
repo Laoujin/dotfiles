e6a04ee5f9dda87fede6a74903c1182302d3af80
Laoujin <woutervs@hotmail.com>
2015-10-07 01:53:05 +0200

    PS: double quotes on cli

--- config/shells/PowerShell/prompt_readline.ps1
+++ config/shells/PowerShell/prompt_readline.ps1
@@ -1,31 +1,39 @@
+# h = Get-History
+
+
 if ($host.Name -eq 'ConsoleHost') {
 	# https://rkeithhill.wordpress.com/2013/10/18/psreadline-a-better-line-editing-experience-for-the-powershell-console/
 	# https://github.com/lzybkr/PSReadLine
 	Import-Module PSReadline
 
-	Set-PSReadLineOption -HistorySearchCursorMovesToEnd 
+	# To enable bash style completion without using Emacs mode, you can use:
+	#Set-PSReadLineOption -HistorySearchCursorMovesToEnd 
+	#Set-PSReadLineOption -EditMode Emacs
+
 	Set-PSReadlineKeyHandler -Key 'UpArrow' -Function 'HistorySearchBackward'
 	Set-PSReadlineKeyHandler -Key 'DownArrow' -Function 'HistorySearchForward'
 
-	Set-PSReadlineKeyHandler -Chord 'Oem7','Shift+Oem7' `
-		-BriefDescription SmartInsertQuote `
-		-LongDescription "Insert paired quotes if not already on a quote" `
-		-ScriptBlock {
+	# https://github.com/lzybkr/PSReadLine/blob/master/PSReadLine/SamplePSReadlineProfile.ps1
+
+	Set-PSReadlineKeyHandler -Key '"',"'" `
+									 -BriefDescription SmartInsertQuote `
+									 -LongDescription "Insert paired quotes if not already on a quote" `
+									 -ScriptBlock {
 		param($key, $arg)
 
 		$line = $null
 		$cursor = $null
-		[Microsoft.PowerShell.PSConsoleReadline]::GetBufferState([ref]$line, [ref]$cursor)
+		[PSConsoleUtilities.PSConsoleReadline]::GetBufferState([ref]$line, [ref]$cursor)
 
 		if ($line[$cursor] -eq $key.KeyChar) {
 			# Just move the cursor
-			[Microsoft.PowerShell.PSConsoleReadline]::SetCursorPosition($cursor + 1)
+			[PSConsoleUtilities.PSConsoleReadline]::SetCursorPosition($cursor + 1)
 		}
 		else {
 			# Insert matching quotes, move cursor to be in between the quotes
-			[Microsoft.PowerShell.PSConsoleReadline]::Insert("$($key.KeyChar)" * 2)
-			[Microsoft.PowerShell.PSConsoleReadline]::GetBufferState([ref]$line, [ref]$cursor)
-			[Microsoft.PowerShell.PSConsoleReadline]::SetCursorPosition($cursor - 1)
+			[PSConsoleUtilities.PSConsoleReadline]::Insert("$($key.KeyChar)" * 2)
+			[PSConsoleUtilities.PSConsoleReadline]::GetBufferState([ref]$line, [ref]$cursor)
+			[PSConsoleUtilities.PSConsoleReadline]::SetCursorPosition($cursor - 1)
 		}
 	}
 }
