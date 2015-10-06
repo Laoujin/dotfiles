54f0c3c56e7230a3eb759a3ec091fd0c0323e9ad
Laoujin <woutervs@hotmail.com>
2015-10-06 02:50:50 +0200

    PS: Moved psreadline to prompt

--- config/shells/Microsoft.PowerShell_profile.ps1
+++ config/shells/Microsoft.PowerShell_profile.ps1
@@ -7,10 +7,8 @@ if (Test-Path ".\Microsoft.PowerShell_aliases.ps1") {
 . ".\PowerShell\envpath.ps1"
 . ".\PowerShell\envpath_mod.ps1"
 . ".\PowerShell\filesystem.ps1"
-. ".\PowerShell\psreadline.ps1"
 
 . ".\PowerShell\prompt.ps1"
-
 Import-Module '.\Modules\Jump.Location\Jump.Location.psd1'
 
 Pop-Location
