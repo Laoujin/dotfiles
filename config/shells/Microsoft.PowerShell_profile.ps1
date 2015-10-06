969614052f9d46a50fffe7cc0b33f4778b6dfd88
Laoujin <woutervs@hotmail.com>
2015-10-06 02:48:25 +0200

    PowerShell: envpath fix

--- config/shells/Microsoft.PowerShell_profile.ps1
+++ config/shells/Microsoft.PowerShell_profile.ps1
@@ -5,10 +5,13 @@ if (Test-Path ".\Microsoft.PowerShell_aliases.ps1") {
 }
 
 . ".\PowerShell\envpath.ps1"
+. ".\PowerShell\envpath_mod.ps1"
 . ".\PowerShell\filesystem.ps1"
 . ".\PowerShell\psreadline.ps1"
 
 . ".\PowerShell\prompt.ps1"
+
+Import-Module '.\Modules\Jump.Location\Jump.Location.psd1'
 
 Pop-Location
 
--- config/shells/PowerShell/envpath.ps1
+++ config/shells/PowerShell/envpath.ps1
@@ -1,9 +1,3 @@
-Append-EnvPath "C:\Program Files\Sublime Text 3"
-Append-EnvPath "C:\Program Files (x86)\MarkdownPad 2"
-
-Set-Alias vgrt vagrant
-
-
 # Reload the $env object from the registry
 # function Refresh-Environment {
 # 	$locations = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment',
