430ab1459bfdf842a40cb661afc03779d8fa4b98
ex02262 <wouter.vanschandevijl@digipolis.be>
2016-01-29 17:55:49 +0100

    PackageManagerConsole: aliases

--- config/shells/NuGet_profile.ps1
+++ config/shells/NuGet_profile.ps1
@@ -1,1 +1,6 @@
 Set-Alias am Add-Migration
+Set-Alias em Enable-Migrations
+Set-Alias ip Install-Package
+Set-Alias unip Uninstall-Package
+Set-Alias ud Update-Database
+Set-Alias udv Update-Database -Verbose
