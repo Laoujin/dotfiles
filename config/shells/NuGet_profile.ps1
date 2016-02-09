37456b003f7d8d0451bd4c05b19f8ae1915ef9e1
ex02262 <wouter.vanschandevijl@digipolis.be>
2016-02-09 12:43:02 +0100

    PowerShell: NuGet profile

--- config/shells/NuGet_profile.ps1
+++ config/shells/NuGet_profile.ps1
@@ -1,6 +1,12 @@
 Set-Alias am Add-Migration
+Set-Alias ad Add-Migration
+
 Set-Alias em Enable-Migrations
 Set-Alias ip Install-Package
 Set-Alias unip Uninstall-Package
 Set-Alias ud Update-Database
-Set-Alias udv Update-Database -Verbose
+
+function Update-DatabaseVerbosely {
+	Update-Database -Verbose
+}
+Set-Alias udv Update-DatabaseVerbosely
