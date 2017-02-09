5a4bf6e3bdf41a460db26145ba0636af91f49eaf
Laoujin <woutervs@hotmail.com>
2017-02-09 17:40:16 +0100

    PowerShell: transed and transde (english->dutch and dutch->english)

--- config/shells/PowerShell/other.ps1
+++ config/shells/PowerShell/other.ps1
@@ -1,2 +1,14 @@
 # npm i -g db-migrate
 Set-Alias dbm db-migrate
+
+# npm i -g google-translate-cli
+function Translate-EnglishToDutch {
+	translate -t nl $args
+}
+function Translate-DutchToEnglish {
+	translate -s nl -t en $args
+}
+
+# Translate English to Dutch
+Set-Alias transed Translate-EnglishToDutch
+Set-Alias transde Translate-DutchToEnglish
