85ff009ebd249e2a6311ad51af2506b7a22a195b
Laoujin <woutervs@hotmail.com>
2018-08-19 05:00:43 +0200

    removed todos

--- todo/tmp/Show-Colors.ps1
+++
@@ -1,8 +0,0 @@
-function Show-Colors( ) {
-  $colors = [Enum]::GetValues( [ConsoleColor] )
-  $max = ($colors | foreach { "$_ ".Length } | Measure-Object -Maximum).Maximum
-  foreach( $color in $colors ) {
-    Write-Host (" {0,2} {1,$max} " -f [int]$color,$color) -NoNewline
-    Write-Host "$color" -Foreground $color
-  }
-}
