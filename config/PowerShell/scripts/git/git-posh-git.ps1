4ce0ebb0a14e856a4a1b33b36c67d8d8bf4c8ba0
Laoujin <woutervs@hotmail.com>
2018-08-29 02:26:57 +0200

    ps git: split into different files

--- config/PowerShell/scripts/git/git-posh-git.ps1
+++ config/PowerShell/scripts/git/git-posh-git.ps1
@@ -1,62 +1,9 @@
 Import-Module Posh-Git
 
-
-# Change colors
+### Change colors
 # Posh-Git v0.x
 # $global:GitPromptSettings.WorkingForegroundColor = [ConsoleColor]::Yellow
 # $global:GitPromptSettings.BranchForegroundColor = [ConsoleColor]::Green
-
-$global:gitStatusNumbers
-
-function Git-NumberedStatus() {
-	$global:gitStatusNumbers = git status -s | % {
-		$file = $_.Substring(3)
-
-		$staged = $_[1] -eq " "
-		$state = If ($staged) {$_[0]} Else {$_[1]}
-		$state = If ($state -eq "??") {"A"} Else {$state}
-		return @{state=$state;file=$file;staged=$staged}
-	}
-
-	$stagedFiles = $global:gitStatusNumbers | Where staged
-	if ($stagedFiles.length) {
-		Write-Host "Staged files:"
-		$stagedFiles | % {
-			Write-Host "    $($_.state) $($_.file)" -ForegroundColor green
-		}
-		Write-Host ""
-	}
-
-	$global:gitStatusNumbers = @($global:gitStatusNumbers | Where {$_.staged -eq $false})
-	if ($global:gitStatusNumbers.length) {
-		Write-Host "Working directory:"
-		$global:gitStatusNumbers | % {$index = -1}{
-			$index++
-
-			$color = switch($_.state) {
-				'A' {'Blue'; break}
-				'M' {'Yellow'; break}
-				'D' {'DarkMagenta'; break}
-				default {'White'}
-			}
-			Write-Host "$index   $($_.state) $($_.file)" -ForegroundColor $color
-		}
-	}
-}
-
-
-function Git-NumberedAdd($index) {
-	# TODO: need to accept 0-3
-	# TODO: need to accept 0 1 5 4 7
-	# TODO: need to accept <4 and >8 ?
-	# TODO: need to quote files
-	$info = $global:gitStatusNumbers[$index]
-	git add $info.file
-	Write-Host "Added $index $($info.file)"
-}
-
-Set-Alias gs Git-NumberedStatus
-Set-Alias ga Git-NumberedAdd
 
 
 # Posh-Git v1.x
@@ -76,8 +23,8 @@ $GitPromptSettings.BranchBehindStatusSymbol.ForegroundColor = 'DarkMagenta'
 $GitPromptSettings.BranchBehindAndAheadStatusSymbol.ForegroundColor = 'Yellow'
 
 
-# Prompt Shrinkers
-$shrinkersPath = Join-Path $PSScriptRoot "..\prompt-aliases.ini"
+### Prompt Shrinkers
+$shrinkersPath = Join-Path $PSScriptRoot "..\..\prompt-aliases.ini"
 # Write-Host "Creating prompt shrinkers as defined in:" -ForegroundColor Blue
 # Write-Host ([System.IO.Path]::GetFullPath($shrinkersPath)) -ForegroundColor White
 $shrinkers = Get-IniContent $shrinkersPath
@@ -108,12 +55,3 @@ $GitPromptSettings.DefaultPromptPath.Text = '$(Get-CustomPromptPath)'
 #     $prompt += Write-Prompt "After" -ForegroundColor ([ConsoleColor]::Magenta)
 #     if ($prompt) { "$prompt " } else { " " }
 # }
-
-
-# I can't type
-Set-Alias gt git
-Set-Alias gut git
-Set-Alias gti git
-Set-Alias got git
-Set-Alias guit git
-Set-Alias giut git
---
+++ config/PowerShell/scripts/git/git-typos.ps1
@@ -0,0 +1,7 @@
+# I can't type
+Set-Alias gt git
+Set-Alias gut git
+Set-Alias gti git
+Set-Alias got git
+Set-Alias guit git
+Set-Alias giut git
