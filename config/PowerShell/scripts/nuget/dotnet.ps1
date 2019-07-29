c7dd9bfb72f31308ecce707dd1944dc42de047c3
Wouter Van Schandevijl <woutervs@hotmail.com>
2019-07-29 11:54:32 +0200

    ps1 profile: added 'cleanbin' == Clean-BinFolders

---
+++ config/PowerShell/scripts/nuget/dotnet.ps1
@@ -0,0 +1,5 @@
+Set-Alias cleanbin Clean-BinFolders
+
+function Clean-BinFolders {
+    Get-ChildItem -Recurse -Directory | Where-Object { $_.Name -eq "bin" -or $_.Name -eq "obj" } | ForEach-Object { Write-Host $_.FullName; npx rimraf $_.FullName }
+}
