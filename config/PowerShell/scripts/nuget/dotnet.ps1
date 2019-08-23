e510749fb2b1c690fde6ec09a73867d68c0689c4
Wouter Van Schandevijl <woutervs@hotmail.com>
2019-08-23 14:58:18 +0200

    ps1: alias cleanbins

--- config/PowerShell/scripts/nuget/dotnet.ps1
+++ config/PowerShell/scripts/nuget/dotnet.ps1
@@ -1,4 +1,4 @@
-Set-Alias cleanbin Clean-BinFolders
+Set-Alias cleanbins Clean-BinFolders
 
 function Clean-BinFolders {
     Get-ChildItem -Recurse -Directory | Where-Object { $_.Name -eq "bin" -or $_.Name -eq "obj" } | ForEach-Object { Write-Host $_.FullName; npx rimraf $_.FullName }
