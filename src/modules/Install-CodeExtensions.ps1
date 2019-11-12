ddaa541fcd6735c93b085846084580930422906b
Wouter Van Schandevijl <wouter.vanschandevijl@partena.be>
2019-11-12 17:59:20 +0100

    VSCode installing extensions with a proxy?

--- src/modules/Install-CodeExtensions.ps1
+++ src/modules/Install-CodeExtensions.ps1
@@ -5,7 +5,14 @@ function Install-CodeExtensions($packages) {
     # C:\Users\%USERNAME%\AppData\Local\Programs\Microsoft VS Code\bin
 
     $path = "C:\Users\$($env:USERNAME)\AppData\Local\Programs\Microsoft VS Code\bin\code.cmd"
+    if (-not (Test-Path $path)) {
+        $path = "C:\Program Files\Microsoft VS Code\bin\Code.cmd"
+    }
+
+    # $proxies = (Get-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings').proxyServer
+    # & $path --proxy-server=http://10.0.25.16:8080
+
 	foreach ($package in $packages) {
-		& $path --install-extension $package
+        & $path --install-extension $package
     }
 }
