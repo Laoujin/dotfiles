3f0567b7131887f144e5dc05506c10a4ccfa3fcf
Wouter Van Schandevijl <woutervs@hotmail.com>
2025-09-19 09:33:01 +0200

    remove sublime text

--- config/PowerShell/scripts/editors.ps1
+++ config/PowerShell/scripts/editors.ps1
@@ -1,5 +1,4 @@
 $editor = "C:\Program Files\Notepad++\notepad++.exe"
-$ide_subl = "C:\Program Files\Sublime Text 3\sublime_text.exe"
 $ide = "C:\Users\$($env:username)\AppData\Local\Programs\Microsoft VS Code\bin\Code.cmd"
 $ide2 = "C:\Program Files\Microsoft VS Code\bin\Code.cmd"
 $markdownEditor = "C:\Program Files (x86)\MarkdownPad 2\MarkdownPad2.exe"
@@ -13,18 +12,6 @@ function Edit-Hosts {
 function Edit-Profile {
 	Start-Process $editor $profile
 }
-
-
-function Start-SublimeText {
-	if ($args.length -eq 0 -or $null -eq $args[0]) {
-		Start-Process $ide_subl
-		return
-	}
-
-	# Fix for GitGutter not working when starting ST3 from the commandline
-	Start-Process $ide_subl $args
-}
-Set-Alias subl Start-VSCode
 
 
 Set-Alias mdp $markdownEditor
