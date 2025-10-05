d3a7092f8c439c8126cd302065a836bdc319cfc8
Wouter Van Schandevijl <woutervs@hotmail.com>
2025-10-05 21:34:44 +0200

    jekyll aliases

--- config/PowerShell/scripts/jekyll.ps1
+++ config/PowerShell/scripts/jekyll.ps1
@@ -1,11 +1,17 @@
 function Start-Jekyll {
-	# TODO: Incremental does not reload on _data changes
-	# TODO: --incremental
-	bundle exec jekyll serve
+	bundle exec jekyll serve --incremental
 }
 function Start-JekyllWithDrafts {
 	bundle exec jekyll serve --drafts --incremental
 }
+function Start-JekyllDev {
+	bundle exec jekyll serve --incremental --config _config_dev.yml
+}
+function Start-JekyllDevWithDrafts {
+	bundle exec jekyll serve --drafts --incremental --config _config_dev.yml
+}
 
 Set-Alias bejs Start-Jekyll
 Set-Alias bejsd Start-JekyllWithDrafts
+Set-Alias bejsdev Start-JekyllDev
+Set-Alias bejsddev Start-JekyllDevWithDrafts
